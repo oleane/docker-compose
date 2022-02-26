cea9e95927a4f3aee955b38b37a1c46f745a0262
oleane <96746343+oleane@users.noreply.github.com>
2022-02-26 05:02:33 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ rm -rf $DESTINATION/.git
 mkdir -p $DESTINATION/postgresql
 sudo chmod -R 777 $DESTINATION
 # config
-if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user>
+if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
 sed -i 's/10014/'$PORT'/g' $DESTINATION/docker-compose.yml
 sed -i 's/20014/'$CHAT'/g' $DESTINATION/docker-compose.yml
